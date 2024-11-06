f1ec43fffe7632538e2a66c4bf0e924af20fc66b
nvv1d <nvv1d@proton.me>
2024-11-06 12:28:25 -0800

    Update submitAssignment.js

--- api/submitAssignment.js
+++ api/submitAssignment.js
@@ -1,6 +1,25 @@
+// pages/api/submitAssignment.js
 import axios from 'axios';
 
-export default async (req, res) => {
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Update with the specific origin if needed
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+async function handler(req, res) {
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method Not Allowed' });
     return;
@@ -13,12 +32,10 @@ export default async (req, res) => {
     return;
   }
 
-  // Replace these with your actual token and chat ID
   const botToken = '7815257977:AAFAMpcCTnr7F-b5-Jy_LKpVegTPIIOUW8k';
   const chatId = '-1002202270365';
 
   try {
-    // Send message to Telegram
     await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
       chat_id: chatId,
       text: message,
@@ -30,4 +47,6 @@ export default async (req, res) => {
     console.error('Error sending message:', error);
     res.status(500).json({ success: false, error: 'Failed to send message' });
   }
-};
+}
+
+export default allowCors(handler);
